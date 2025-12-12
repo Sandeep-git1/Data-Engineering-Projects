6388cb659e8df3e3efc82e8b3c7f1de35f23bdae
Sandeep-git1 <walace1316.sp@gmail.com>
2025-12-13 04:24:57 +0530

    Implement bulk insert for CRM and ERP tables
    Added bulk insert operations for multiple tables and included truncate statements.

--- sql-data-warehouse-project/scripts/init_database.sql
+++ sql-data-warehouse-project/scripts/init_database.sql
@@ -104,3 +104,83 @@ cat NVARCHAR(50),
 subcat NVARCHAR(50),
 maintenance NVARCHAR(50)
 );
+
+
+/* to rename if there is an spelling error-
+EXEC sp_rename 'bronze.crm_cust_ifno', 'crm_cust_info';
+*/
+
+/* to alter table if you input wrong datatype - 
+ALTER TABLE bronze.erp_cust_az12
+ALTER COLUMN cid VARCHAR(50);
+*/
+
+/* # this is the sql script to use "truncate" and "full load" the data */
+use DataWarehouse;
+
+TRUNCATE TABLE bronze.crm_cust_info;
+BULK INSERT bronze.crm_cust_info
+from 'C:\project_\Data Engineering\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+with (
+ firstrow = 2,
+ FIELDTERMINATOR = ',',
+ tablock
+);
+
+SELECT COUNT(*) FROM bronze.crm_cust_info;
+
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+from 'C:\project_\Data Engineering\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+with (
+ firstrow = 2,
+ FIELDTERMINATOR = ',',
+ tablock
+);
+
+SELECT COUNT(*) FROM bronze.crm_cust_info;
+
+
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
+from 'C:\project_\Data Engineering\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+with (
+ firstrow = 2,
+ FIELDTERMINATOR = ',',
+ tablock
+);
+SELECT COUNT(*) FROM bronze.crm_sales_details;
+
+ALTER TABLE bronze.erp_cust_az12
+ALTER COLUMN cid VARCHAR(50);
+
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\project_\Data Engineering\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+SELECT COUNT(*) FROM bronze.erp_cust_az12;
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\project_\Data Engineering\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+SELECT COUNT(*) FROM bronze.erp_loc_a101;
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\project_\Data Engineering\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2;
